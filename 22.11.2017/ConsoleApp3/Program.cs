65c35e8fd937031c15d0e2b722d848f65b2bab7e
AgeDee <agedee34@gmail.com>
2017-12-05 17:01:34 +0100

    aktualizacja

---
+++ 22.11.2017/ConsoleApp3/Program.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net;
+using System.Net.Sockets;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ConsoleApp3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            IPAddress adress = IPAddress.Parse("127.0.0.1");
+            int port = 2048;
+
+            Server server = new Server(adress, port);
+            var sTask = server.serverTask();
+
+
+            try
+            {
+                //server.Address = IPAddress.Parse("127.0.0.4");
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message, "error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+
+            try
+            {
+                //server.Port = 8080;
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message, "error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+            Task.WaitAll(sTask);
+        }
+
+    }
+
+    class Server
+    {
+        IPAddress address;
+        int port;
+        bool running;
+
+        public Server(IPAddress adress, int port)
+        {
+            this.Address = adress;
+            this.Port = port;
+        }
+        
+        public int Port { get => port; set { if (!running) port = value; else throw new Exception("Nie można zmienic portu, kiedy serwer jest uruchomiony."); } }
+        public IPAddress Address { get => address; set { if (!running) address = value; else throw new Exception("Nie można zmienić adresu IP, kiedy serwer jest uruchomiony."); } }
+        public bool Running { get => running; set => running = value; }
+
+        public async Task serverTask()
+        {
+            TcpListener server = new TcpListener(Address, Port);
+            server.Start();
+            running = true;
+            while (true)
+            {
+                TcpClient client = await server.AcceptTcpClientAsync();
+                byte[] buffer = new byte[1024];
+                await client.GetStream().ReadAsync(buffer, 0, buffer.Length).ContinueWith(
+                    async (t) =>
+                    {
+                        int i = t.Result;
+                        LogServera log = new LogServera();
+                        while (true)
+                        {
+                            client.GetStream().WriteAsync(buffer, 0, i);
+                            i = await client.GetStream().ReadAsync(buffer, 0, buffer.Length);
+                            log.add(buffer);
+                            log.print();
+                        }
+                    });
+            }
+        }
+    }
+
+    class LogServera
+    {
+        List<byte[]> log = new List<byte[]>();
+
+        public void add(byte[] buff)
+        {
+            log.Add(buff);
+        }
+
+        public void print()
+        {
+            string result;
+
+            log.ForEach(delegate (byte[] b)
+            {
+                result = System.Text.Encoding.UTF8.GetString(b);
+                Console.WriteLine(result);
+            });    
+        }
+    }
+
+}
---
+++ ConsoleApp8/ConsoleApp8/Program.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace ConsoleApp7
+{
+    class Program
+    {
+        delegate int DelegateType(int n);
+        static DelegateType delegateSilniaIt;
+        static DelegateType delegateSilniaRek;
+        static DelegateType delegateFibo;
+
+        static int silniait(int n)
+        {
+            int wynik = 1;
+            for (int i = 1; i<n; i++)
+            {
+                wynik *= i;
+            }
+
+            return wynik;
+        }
+
+        static int silniarek(int n)
+        {
+            if (n == 0 || n == 1)
+            {
+                return 1;
+            }
+            else
+            {
+                return silniarek(n - 1) * n;
+            }
+        }
+
+        static int fibo(int n)
+        {
+            int fib = 0;
+
+            for(int i = 0; i < n; i++)
+            {
+                fib += i;
+            }
+            return fib;
+        }
+
+        static void Main(string[] args)
+        {
+            delegateSilniaIt = new DelegateType(silniait);
+            delegateSilniaRek = new DelegateType(silniarek);
+            delegateFibo = new DelegateType(fibo);
+
+            IAsyncResult ar = delegateSilniaIt.BeginInvoke(7, null, null);
+            IAsyncResult ar2 = delegateSilniaRek.BeginInvoke(7, null, null);
+            IAsyncResult ar3 = delegateFibo.BeginInvoke(7, null, null);
+
+            int result1 = delegateSilniaIt.EndInvoke(ar);
+            int result2 = delegateSilniaRek.EndInvoke(ar2);
+            int result3 = delegateFibo.EndInvoke(ar3);
+
+            //Console.WriteLine(result1);
+            Console.WriteLine(result2);
+            //Console.WriteLine(result3);
+            Thread.Sleep(5000);
+        }
+
+    }
+}
